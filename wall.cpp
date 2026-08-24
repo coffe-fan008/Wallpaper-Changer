de6a0f4d186db42c15486f5c263b434cdf8b391d
Emi <staypuff898@gmail.com>
2026-08-24 00:27:50 -0700

    Update wall.cpp
    i fix a error that breaks the program in the function that check if folder exist, and i add "cstdlib" because i forgotten it ; Also i will add a file that stores the wallpapers path and i will add a text-based user interface (TUI) 
    : )

--- wall.cpp
+++ wall.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
 #include <filesystem>
-
+#include <cstdlib>
 using namespace std; 
 
 int main () {
+  //create a variable to save the use name and one variable to the wallpapers folder and other raw variable to use with cin to save the wallpaper name
   const char* user = getenv("USER");
   string something = "/home/" + string(user) + "/Pictures/Wallpapers";
   string wall;
+
+  //clear terminal and print wallpaper on ascii art
   cout<<("\033[2J\033[1H");
       cout<<R"(█▀▀▀█ ▀▒ ▀█  ▄▀▀▀▀▄  █▀▀▓     █▀▀▓     █▀▀▀▀▀▄   ▄▀▀▀▀▄  █▀▀▀▀▀▄   ▄▀▀▀▀▀█ █▀▀▀▀▀▄ 
 █   ▓  ░  ░ █      █ █  ░     █  ░     █      █ █      █ █      █ █      ▓ █      █
@@ -15,7 +18,9 @@ int main () {
 ▓   ░  ▓  ▓ ▓  ▀▀  ▓ ▓  █▄▀▀█ ▓  █▄▀▀█ ▓  ▀▀ ▄▀ ▓  ▀▀  ▓ ▓  ▀▀ ▄▀ ▓  █▄█▄▄ ▓  ▀▀ ▀▄
 ▒         ▒ ▒  █   ▒ ▒  ▀▀  ▒ ▒  ▀▀  ▒ ▒  █▀▀   ▒  █   ▒ ▒  █▀▀   ▒      ▒ ▒  █   ▒
  ▀▄▄▄▀▄▄▄▀  ░▄▄█ ▄▄░  ▀▄▄▄▄▀   ▀▄▄▄▄▀  ░▄▄█     ░▄▄█ ▄▄░ ░▄▄█      ▀▄▄▄▄▄█ ░▄▄█ ▄▄█)"<<'\n';
-  {
+  
+  //check if the dir /home/user/Pictures/Wallpapers/ exist
+    {
     for (auto const str:
     {
         "/home/" + string(user) + "/Pictures/Wallpapers/",
@@ -23,14 +28,24 @@ int main () {
     {
         std::filesystem::directory_entry entr7{str};
 
-        std::cout << '\n' << "Directory: " << entr7<< '\n'
-                  << (entr7.exists() ? "\n" : " doesn't exist; please create it and place your wallpapers there.\n" );
+            if (entr7.exists() ? filesystem::exists(entr7) : filesystem::exists(entr7))
+        std::cout << "Directory " << entr7 << '\n' << '\n';
+    else {
+        std::cout << "doesn't exist; please create it and place your wallpapers there\n";
+        return 1;
+     }
     }
 }
+
+//check files and sub files on the folder and print the names
   for (const auto & entry : filesystem::recursive_directory_iterator(something))
   cout << entry.path().filename()<<'\n';
+
+  //get wallpaper name 
   cout<<'\n'<<"Put your Image name and extension"<<'\n'<<"> ";
   getline(cin, wall);
+
+  //send command to change wallpaper, color palette and clear terminal then in shows the saitma ascii art
   system((string("awww img --transition-type=random /home/") + string(user) + ("/Pictures/Wallpapers/") + wall.c_str()).c_str());
   system((std::string("wal -n -i /home/") + string(user) + ("/Pictures/Wallpapers/") + wall.c_str()).c_str());
   cout<<("\033[2J\033[1H");
